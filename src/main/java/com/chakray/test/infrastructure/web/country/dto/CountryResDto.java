240f37fa7e06513b34b5eb12e1b85926bbdc9db6
Ronaldo Quero Santiago <ronaldo.quero.santiago@gmail.com>
2026-02-22 17:09:57 -0600

    docs: add example schema for CountryResDto in Swagger

--- src/main/java/com/chakray/test/infrastructure/web/country/dto/CountryResDto.java
+++ src/main/java/com/chakray/test/infrastructure/web/country/dto/CountryResDto.java
@@ -1,11 +1,15 @@
 package com.chakray.test.infrastructure.web.country.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class CountryResDto {
+    @Schema(description = "Country code", example = "MEX")
     private String code;
+
+    @Schema(description = "Country name", example = "Mexico")
     private String name;
 }
